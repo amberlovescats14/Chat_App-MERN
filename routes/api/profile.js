9766b67498c2ff63deeb315b5fb6922ffd84384d
Amber Jones <amberlovescats14@gmail.com>
2019-06-24 22:49:13 -0500

    likes and unlike

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -250,15 +250,34 @@ router.delete('/education/:edu_id', auth, async(req, res)=> {
 //! @route    GET api/profile/github/:username
 //! @desc     get user repos from github
 //! @access   public
-router.get('/github/:username', async(req, res)=> {
+// @route    GET api/profile/github/:username
+// @desc     Get user repos from Github
+// @access   Public
+router.get('/github/:username', (req, res) => {
   try {
-    const option = {
-      uri: `https://api/github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientID')}&client_secret=${config.get('githubSecret')}`
-    }
-    
-  } catch (error) {
-    console.error(error.message)
-    res.status(500).send(`Server Error`)
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        'githubClientID'
+      )}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' }
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: 'No Github profile found' });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
   }
-})
+});
+
 module.exports = router
